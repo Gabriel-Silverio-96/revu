856fe7bb56f0d0d0f1bc3638f458d24f1f07ff0a
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2025-04-13 20:47:01 -0300

    feat: create get started page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,61 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Link } from "expo-router";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
+  const breakLine = "\n";
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <Image
+        style={styles.backgroundImage}
+        source={require("@/assets/images/background-get-started.png")}
+      />
+
+      <Text style={styles.title}>
+        Review {breakLine}remember and {breakLine}repeat
+      </Text>
+      <Text style={styles.description}>
+        Your daily companion to smarter,{breakLine} faster learning.
+      </Text>
+      <View style={styles.action}>
+        <Link href="/explore" style={styles.button}>
+          <Text style={{ color: "#fff", fontWeight: "bold" }}>Get started</Text>
+        </Link>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    marginTop: 100,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  backgroundImage: {
+    height: 346,
+    width: 346,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  title: {
+    fontSize: 40,
+    fontWeight: "bold",
+    textAlign: "center",
+    lineHeight: 45,
+  },
+  description: {
+    textAlign: "center",
+    color: "#A1A1A1",
+    fontSize: 13,
+    marginTop: 15,
+  },
+  button: {
+    width: 174,
+    padding: 15,
+    textAlign: "center",
+    backgroundColor: "#7C87FF",
+    borderRadius: 9,
+  },
+  action: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 30,
   },
 });
